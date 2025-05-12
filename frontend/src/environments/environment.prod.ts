23af27bbd0757dec482e062f4d7ab737ccf353bf
jimonte30 <jherickisamar@gmail.com>
2025-05-12 14:58:27 +0800

    Adding configuration to connect to real deployment website

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://final-intprog-project-eqiv.onrender.com',
-    allowedDomains: ['final-intprog-project-eqiv.onrender.com'],
+    apiUrl: 'https://final-intprog-project-ifwg.onrender.com',
+    allowedDomains: ['final-intprog-project-ifwg.onrender.com'],
     disallowedRoutes: []
 }
